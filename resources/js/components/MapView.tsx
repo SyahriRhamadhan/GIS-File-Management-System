a91933f091fba8fd3c4fd18ea1ab5b67e754a5ec
SyahriRhamadhan <2001020038@student.umrah.ac.id>
2025-05-20 15:11:18 +0700

    component baselayer

--- resources/js/components/MapView.tsx
+++ resources/js/components/MapView.tsx
@@ -8,6 +8,7 @@ import { FaFilePdf } from 'react-icons/fa6';
 import { IoAddCircleOutline } from 'react-icons/io5';
 import { GeoJSON, LayersControl, MapContainer, Popup, ScaleControl, TileLayer } from 'react-leaflet';
 import GeomanControl from './GeomanControl';
+import BaseLayers from '@/components/BaseLayer'; 
 
 const { BaseLayer, Overlay } = LayersControl;
 
@@ -169,64 +170,7 @@ const MapView: React.FC<MapViewProps> = ({ geojsonData }) => {
                     <GeomanControl />
 
                     <LayersControl position="topright">
-                        <BaseLayer checked name="Esri Satellite">
-                            <TileLayer
-                                url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-                                attribution="Tiles &copy; Esri — Source: Esri, USDA, USGS"
-                            />
-                        </BaseLayer>
-                        <BaseLayer name="OSM Standard">
-                            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap contributors" />
-                        </BaseLayer>
-                        <BaseLayer name="Stamen Toner">
-                            <TileLayer
-                                url="https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png"
-                                attribution={`
-    &copy; <a href="https://stadiamaps.com/" target="_blank" rel="noreferrer">Stadia Maps</a> 
-    &copy; <a href="https://stamen.com/" target="_blank" rel="noreferrer">Stamen Design</a> 
-    &copy; <a href="https://openmaptiles.org/" target="_blank" rel="noreferrer">OpenMapTiles</a> 
-    &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noreferrer">OpenStreetMap</a>
-    `}
-                            />
-                        </BaseLayer>
-                        <BaseLayer name="Stamen Terrain">
-                            <TileLayer
-                                url="https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png"
-                                maxZoom={20}
-                                attribution={`
-    &copy; <a href="https://stadiamaps.com/" target="_blank" rel="noreferrer">Stadia Maps</a>
-    &copy; <a href="https://stamen.com/" target="_blank" rel="noreferrer">Stamen Design</a>
-    &copy; <a href="https://openmaptiles.org/" target="_blank" rel="noreferrer">OpenMapTiles</a>
-    &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noreferrer">OpenStreetMap</a>
-  `}
-                            />
-                        </BaseLayer>
-                        <BaseLayer name="Carto Positron">
-                            <TileLayer
-                                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                                attribution="&copy; CARTO &copy; OpenStreetMap"
-                            />
-                        </BaseLayer>
-                        <BaseLayer name="Carto Dark">
-                            <TileLayer
-                                url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-                                attribution="&copy; CARTO &copy; OpenStreetMap"
-                            />
-                        </BaseLayer>
-                        <BaseLayer name="OpenTopoMap">
-                            <TileLayer
-                                url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
-                                attribution="Map data: &copy; OpenStreetMap contributors, SRTM | Map style: &copy; OpenTopoMap"
-                            />
-                        </BaseLayer>
-                        <BaseLayer name="NASA City Lights">
-                            <TileLayer
-                                url="https://gibs.earthdata.nasa.gov/wmts/epsg3857/best/VIIRS_CityLights_2012/default/2012-01-01/GoogleMapsCompatible_Level8/{z}/{y}/{x}.jpg"
-                                attribution="Imagery courtesy NASA EOSDIS GIBS"
-                                maxNativeZoom={8}
-                                maxZoom={18}
-                            />
-                        </BaseLayer>
+                        <BaseLayers />
 
                         {/* Overlay filtered */}
                         {uniqueSourceNames.map((sourceName) => {
