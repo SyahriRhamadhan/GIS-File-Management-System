678972025f66d5b2225447b9e8f3f0acb611f823
SyahriRhamadhan <2001020038@student.umrah.ac.id>
2025-06-13 15:11:55 +0700

    fix dikit

--- resources/js/pages/landing/navbar.tsx
+++ resources/js/pages/landing/navbar.tsx
@@ -138,7 +138,6 @@ export default function Navbar() {
                                     Register
                                 </Link> */}
                         </>
-                        ){'}'}
                     </li>
                 </ul>
             </nav>
